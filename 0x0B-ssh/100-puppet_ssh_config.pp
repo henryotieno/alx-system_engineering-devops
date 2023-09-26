84b2d5e7a1b8ef08267f967500d6240d9636ecfa
Henry Nyobange <henrynyobange@gmail.com>
2023-09-27 01:24:19 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,11 @@
-sh config
+# Configure the ssh_config file to allow access to my ubuntu server with out requesting for a private key
 
-file_line{'Turn off passwd auth':
-path => '/etc/ssh/ssh_config',
-line => 'PasswordAuthentication no'
+file_line { 'Change the main private key':
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/school',
 }
 
-file_line{'Declare identity file':
-path => '/etc/ssh/ssh_config',
-line => 'IdentityFile ~/.ssh/school'
-
+file_line { 'No Authenticate with password':
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
 }
